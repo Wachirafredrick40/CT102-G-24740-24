ae6cd549a731315833890375a0c43b9a96add548
Wachirafredrick40 <mungaifredrick80@gmail.com>
2024-10-17 22:48:02 +0300

    Add files via upload

---
+++ 1d.c
@@ -0,0 +1,29 @@
+/*
+program to calculate electric balance using user defined function
+author:Fredrick wachira
+reg no:CT10/G/24740/24
+*/
+
+
+#include <stdio.h>
+int main(){
+
+int i,j;
+int marks[2][3]={{1,2,3},{4,5,6}};
+for(i=0;i<2;i++){
+for(j=0;j<3;j++){
+printf("marks [%d][%d]=%d\n",i,j,marks[i][j]);
+}
+}
+
+
+
+
+
+
+
+
+
+
+return 0;
+}
