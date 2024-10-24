42eed5e0e14980b2029d4890192619aab982718f
Wachirafredrick40 <mungaifredrick80@gmail.com>
2024-10-24 23:16:55 +0300

    Add files via upload

---
+++ do while.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+	int number;
+	//prompt the user to enter number
+	printf("enter te number:");
+	scanf("%d",&number);
+	
+	do{
+		printf("the value of number=%d\n",number);
+		number ++;
+		
+	}
+	while (number<20);
+	
+	retu
